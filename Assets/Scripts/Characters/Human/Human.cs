c4f1c21c77abf8abd0dfdc34d982764164beb2ea
HawkeMaylan <71107545+HawkeMaylan@users.noreply.github.com>
2025-06-06 01:05:18 -0400

    Cooldown Changed for Acoustic Flare

--- Assets/Scripts/Characters/Human/Human.cs
+++ Assets/Scripts/Characters/Human/Human.cs
@@ -2711,7 +2711,7 @@ namespace Characters
             itemList1.Add(new FlareItem(this, "Cyan", new Color(0f, 255f / 252, 255f / 255, 0.8f), 300f));
             itemList1.Add(new FlareItem(this, "Indigo", Color.blue, 300f));
             itemList2.Add(new FlareItem1(this, "Flash Flare", Color.white, 220f));
-            itemList2.Add(new FlareItem3(this, "Acoustic Flare", Color.grey, 1f));
+            itemList2.Add(new FlareItem3(this, "Acoustic Flare", Color.grey, 300f));
             itemList2.Add(new HorseWhistleItem(this, "Whistle", 5f));        
             
 
