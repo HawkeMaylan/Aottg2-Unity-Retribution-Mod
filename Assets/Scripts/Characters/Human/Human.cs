c1f74ebed35f9c6768696b3b5fddd7071cf26741
HawkeMaylan <71107545+HawkeMaylan@users.noreply.github.com>
2025-06-14 02:26:18 -0400

    Beyond Varreosa

--- Assets/Scripts/Characters/Human/Human.cs
+++ Assets/Scripts/Characters/Human/Human.cs
@@ -2791,7 +2791,10 @@ namespace Characters
                 itemList4.Add(new ShigGateSpawn(this, "ShigGateSpawn", 1f));
                 itemList4.Add(new PickupGroundCannon(this, "Field Cannon Item", 1f));
                 itemList4.Add(new Stable2Spawn(this, "Stables", 1f));
+                itemList4.Add(new PastVarreosaSpawn(this, "PastVarreosa", 1f));
                 
+
+
 
 
 
--- Assets/Scripts/Characters/Human/Items/PastVarreosaSpawn.cs
+++ Assets/Scripts/Characters/Human/Items/PastVarreosaSpawn.cs
@@ -4,9 +4,9 @@ using Photon.Pun;
 
 namespace Characters
 {
-    class ShigGateSpawn : SimpleUseable
+    class PastVarreosaSpawn : SimpleUseable
     {
-        public ShigGateSpawn(BaseCharacter owner, string name, float cooldown) : base(owner)
+        public PastVarreosaSpawn(BaseCharacter owner, string name, float cooldown) : base(owner)
         {
             Name = name;
             Cooldown = cooldown;
@@ -23,7 +23,8 @@ namespace Characters
                 Vector3 pos = Vector3.zero; // Always spawn at (0, 0, 0)
                 GameObject ShigGateN = PhotonNetwork.Instantiate("Buildables/ShigGateNorth", pos, Quaternion.identity);
                 GameObject ShigGateS = PhotonNetwork.Instantiate("Buildables/ShigGateSouth", pos, Quaternion.identity);
-               /// GameObject WaterBase = PhotonNetwork.Instantiate("Buildables/WaterEmpty", pos, Quaternion.identity);
+                GameObject Varreosa = PhotonNetwork.Instantiate("Buildables/PastVarreosa", pos, Quaternion.identity);
+                /// GameObject WaterBase = PhotonNetwork.Instantiate("Buildables/WaterEmpty", pos, Quaternion.identity);
             }
             catch { /* Silently ignore errors if any occur */ }
         }
