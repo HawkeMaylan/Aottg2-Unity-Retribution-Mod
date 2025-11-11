9b309aa895deb28ebcf811735f1d3619547d8e4a
HawkeMaylan <71107545+HawkeMaylan@users.noreply.github.com>
2025-11-11 17:00:10 -0500

    Revert "Sheathe can be done on last blade"
    This reverts commit 0fe366d30eba433bf474452018dd74d6f38924ea.

--- Assets/Scripts/Characters/Human/Human.cs
+++ Assets/Scripts/Characters/Human/Human.cs
@@ -1066,7 +1066,8 @@ namespace Characters
             }
             else if (Weapon is BladeWeapon)
             {
-
+                if (((BladeWeapon)Weapon).BladesLeft <= 0)
+                    return;
                 ToggleBlades(false);
                 if (Grounded)
                     PlaySound(HumanSounds.BladeReloadGround);
